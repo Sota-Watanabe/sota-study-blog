228617cfdec6bd003c86168c035323985500a869
Sota-Watanabe <stronger.riceball@gmail.com>
2022-05-23 19:18:47 +0900

    editorモードのアイコン追加

--- sota-blog/client/components/MyHeader.vue
+++ sota-blog/client/components/MyHeader.vue
@@ -4,8 +4,12 @@
       <p class="h-[100px] w-[189px] text-[22px] ml-8 leading-[100px]">
         {{ getHeaderName }}
       </p>
-      <div class="m-auto relative mr-[47px] menu">
+      <div v-if="!editMode" class="m-auto relative mr-[47px] menu">
         <p>BLOG</p>
+      </div>
+      <div v-else class="flex items-center m-auto mr-1">
+        <img src="~/assets/plus.png" class="w-[29.1px] h-[29.1px] " />
+        <img src="~/assets/home.svg" class="w-[41px] h-[35px] mx-[52px]" />
       </div>
     </section>
     <div class="drop-shadow-md" />
