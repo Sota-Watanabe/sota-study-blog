459b39f0383a1199f8d598a4a50bbd0dfcb3e2f1
Sota-Watanabe <stronger.riceball@gmail.com>
2022-05-24 19:58:18 +0900

    アップロードの動作確認

--- sota-blog/client/pages/edit-editor/new.vue
+++ sota-blog/client/pages/edit-editor/new.vue
@@ -2,9 +2,13 @@
   <div>
     <my-header :editMode="true" />
     <div class="flex flex-col mt-[78px] mx-[13%] max-w-[1063px] pb-[91px]">
-      <div class="w-full h-dammy border-solid border-[1px] border-black text-center line-h-dammy align-center" >
+      <label
+        class="w-full h-dammy border-solid border-[1px] border-black text-center line-h-dammy align-center"
+      >
+        <input ref="file" type="file" @change="upload" class="w-full hidden" />
         no image
-      </div>
+      </label>
+      <img :src="value" />
       <ul class="mt-[56px]">
         <li class="flex items-center">
           <p class="text-[18px]">キャッチイメージ:</p>
@@ -26,15 +30,24 @@
         </li>
         <li class="flex items-center mt-[43px]">
           <p class="w-[133px] text-[18px]">公開/非公開:</p>
-          <my-button :isActive="true" text="公開" />
-          <my-button :isActive="false" text="非公開" />
+          <my-button
+            @click.native="isPublic = true"
+            :isActive="isPublic"
+            text="公開"
+          />
+          <my-button
+            @click.native="isPublic = false"
+            :isActive="!isPublic"
+            text="非公開"
+            class="ml-[34px]"
+          />
         </li>
         <li class="flex items-center mt-[43px]">
           <p class="w-[95px] text-[18px]">更新日:</p>
           <the-time />
         </li>
       </ul>
-      <div class="flex gap-[37px] mt-[90px] ">
+      <div class="flex gap-[37px] mt-[90px]">
         <my-button :isActive="false" text="保存" />
         <my-button :isActive="false" text="非公開" />
       </div>
@@ -63,5 +76,61 @@ import MyButton from "../../components/MyButton.vue";
 export default Vue.extend({
   name: "IndexPage",
   components: { MyHeader, MyFooter, ArticleBoard, PrevNext, TheTime, MyButton },
+  props: [],
+  data() {
+    return {
+      isPublic: true,
+      counter: 0,
+      file: null,
+      value: ""
+    };
+  },
+  methods: {
+    // https://qiita.com/itoshiki/items/511d58b827f4ce2129fc 参考
+    async upload(event: Event) {
+      //  if (event.target instanceof HTMLInputElement == false
+      // //  event.target == null ||
+      // //  event.target.files ==null
+      //  ) return
+      //  if (event.target == null) return
+      const files = (event.target as HTMLInputElement).files;
+      if (files == null) return;
+      const file = files[0];
+
+      if (this.checkFile(file)) {
+        const picture = await this.getBase64(file);
+        this.$emit("input", picture);
+        console.log(typeof(picture))
+        if (typeof(picture) != 'string') return
+        this.value = picture;
+      }
+    },
+    getBase64(file: File): Promise<string | ArrayBuffer | null> {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
+      });
+    },
+    checkFile(file: File) {
+      console.log(`file=${file}`)
+      let result = true;
+      const SIZE_LIMIT = 5000000; // 5MB
+      // キャンセルしたら処理中断
+      if (!file) {
+        result = false;
+      }
+      // jpeg か png 関連ファイル以外は受付けない
+      if (file.type !== "image/jpeg" && file.type !== "image/png") {
+        result = false;
+      }
+      // 上限サイズより大きければ受付けない
+      if (file.size > SIZE_LIMIT) {
+        result = false;
+      }
+      return result;
+    },
+  },
 });
 </script>
