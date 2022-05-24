05f264f702e9247ab48fc9e8ec7f479209a8abc7
Sota-Watanabe <stronger.riceball@gmail.com>
2022-05-24 15:39:46 +0900

    MyButtonコンポーネントの追加

---
+++ sota-blog/client/components/MyButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input
+      v-if="isYellow"
+      type="button"
+      :value="text"
+      class="w-[93px] h-[51px] mr-[34px] text-[18px] bg-[#FFB800] rounded-[18px]"
+    />
+    <input
+      v-else
+      type="button"
+      :value="text"
+      class="w-[93px] h-[51px] bg-transparent text-[18px] border-solid border-black border-[1px] rounded-[18px]"
+    />
+  </div>
+</template>
+
+<style scoped></style>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  name: "MyButton",
+  props: ["isYellow", "text"],
+  data() {
+    return {};
+  }
+});
+</script>
