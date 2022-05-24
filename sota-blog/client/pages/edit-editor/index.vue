e67dbf412d6daddd6fdcd2db1c933b629a11ab2b
Sota-Watanabe <stronger.riceball@gmail.com>
2022-05-24 16:05:16 +0900

    ファイル名変更

--- sota-blog/client/pages/edit-editor/index.vue
+++ sota-blog/client/pages/edit-editor/index.vue
@@ -45,12 +45,12 @@
 
 <script lang="ts">
 import Vue from "vue";
-import MyHeader from "../components/MyHeader.vue";
-import MyFooter from "../components/MyFooter.vue";
-import ArticleBoard from "../components/ArticleBoard.vue";
-import PrevNext from "../components/PrevNext.vue";
-import TheTime from "../components/TheTime.vue";
-import MyButton from "../components/MyButton.vue";
+import MyHeader from "../../components/MyHeader.vue";
+import MyFooter from "../../components/MyFooter.vue";
+import ArticleBoard from "../../components/ArticleBoard.vue";
+import PrevNext from "../../components/PrevNext.vue";
+import TheTime from "../../components/TheTime.vue";
+import MyButton from "../../components/MyButton.vue";
 export default Vue.extend({
   name: "IndexPage",
   components: { MyHeader, MyFooter, ArticleBoard, PrevNext, TheTime, MyButton },
